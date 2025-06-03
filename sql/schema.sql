c88a6174961ba3563ae7d6d55e3a28c5726ab8a3
CleverXiaobaii <cleverxiaobai1@outlook.com>
2025-06-03 15:01:29 +0200

    Add files via upload

---
+++ sql/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS rawdata_from_sensors (
+    id SERIAL PRIMARY KEY,
+    sensor_id INTEGER NOT NULL,
+    plant_id INTEGER NOT NULL,
+    sensor_name VARCHAR(50) UNIQUE NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    temperature FLOAT NOT NULL,
+    humidity FLOAT NOT NULL,
+    soil_moisture FLOAT NOT NULL,
+    is_anomaly BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS plants(
+    id SERIAL PRIMARY KEY,
+    plant_name VARCHAR(50) UNIQUE NOT NULL
+);

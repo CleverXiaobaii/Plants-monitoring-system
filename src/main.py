5e70b21b7bf461d8320d995d72c5dcbff7af8bb2
CleverXiaobaii <cleverxiaobai1@outlook.com>
2025-06-06 09:45:16 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
+import threading
+import listen
+
 from database import db_manager
 
 def main():
     print("programme running...")
-    
     # 初始化数据库
     if not db_manager.initialize_database():
         print("❌ initializing failed, programme exits")
@@ -11,6 +13,10 @@ def main():
     # 主程序逻辑
     print("🖥️ 应用程序运行中...")
     # 你的业务逻辑代码
+    thread_listen = threading.Thread(target=listen.listening())
+    #thread2 = threading.Thread(target=worker)
+
+    thread_listen.start()
     
     print("🛑 应用程序结束")
 

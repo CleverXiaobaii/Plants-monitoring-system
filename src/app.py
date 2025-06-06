2b4fdbfce76b35d96e6eaea34222567363b06d23
Chikara Matsuzawa <waicanth0730@gmail.com>
2025-06-06 16:24:56 +0200

    implement summary api

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 # app.py
 from flask import Flask, jsonify, request, render_template
 import psycopg2
-from calc import (
-    get_db_connection,
-    fetch_sensor_data
-)
+from config import config
+
+def get_db_connection():
+    return psycopg2.connect(config.DB_URL)
 
 app = Flask(__name__)
 
@@ -17,7 +17,8 @@ def sensor_data():
     conn = get_db_connection()
     cur = conn.cursor()
     try:
-        data = fetch_sensor_data(cur)
+        cur.execute("SELECT * FROM rawdata_from_sensors;")
+        data = cur.fetchall()
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     finally:
@@ -56,6 +57,41 @@ def latest_data(sensor_id):
         conn.close()
 
     return jsonify(sensor_data)
+
+@app.route('/summary', methods=['GET']) # return average temperature
+def avg_temp():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute("""
+            WITH latest_per_sensor AS (
+                SELECT DISTINCT ON (sensor_id) *
+                FROM rawdata_from_sensors
+                ORDER BY sensor_id, time_stamp DESC
+            )
+            SELECT
+                AVG(temperature) AS avg_temp,
+                AVG(humidity) AS avg_humidity,
+                AVG(soil_moisture) AS avg_soil_moisture,
+                SUM(CASE WHEN is_anomaly THEN 1 ELSE 0 END) AS anomaly_count
+            FROM latest_per_sensor;
+        """)
+        row = cur.fetchone()
+        if row is None or all(val is None for val in row):
+            return jsonify({"error": "Data unavailable"}), 404
+        result = {
+            "avg_temp": row[0],
+            "avg_humidity": row[1],
+            "avg_soil_moisture": row[2],
+            "anomaly_count": row[3]
+        }
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        cur.close()
+        conn.close()
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)

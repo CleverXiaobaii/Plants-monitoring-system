ec8f4ed083b16007d905f9d40267b619d73da77f
Chikara Matsuzawa <waicanth0730@gmail.com>
2025-06-06 15:51:45 +0200

    implement latest api

--- src/app.py
+++ src/app.py
@@ -26,7 +26,36 @@ def sensor_data():
         
     return jsonify(data)
 
+@app.route('/latest/<int:sensor_id>', methods=['GET']) # return latest data by sensor id
+def latest_data(sensor_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute("""
+            SELECT *
+            FROM rawdata_from_sensors
+            WHERE sensor_id = %s
+            ORDER BY time_stamp DESC
+            LIMIT 1;
+        """, (sensor_id,))
+        row = cur.fetchone()
+        if row is None:
+            return jsonify({"error": "No data found"}), 404
+        sensor_data = {
+            "sensor_id": row[1],
+            "time_stamp": row[2],
+            "temperature": row[3],
+            "humidity": row[4],
+            "soil_moisture": row[5],
+            "is_anomaly": row[6]
+        }
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        cur.close()
+        conn.close()
 
+    return jsonify(sensor_data)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
